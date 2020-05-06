c91cb085fd52285d772ae6ab16fb6557071a0227
Angel <57370596+angelmlucero@users.noreply.github.com>
2020-05-06 02:31:15 -0300

    Add files via upload

--- alarma.js
+++ alarma.js
@@ -1,15 +1,62 @@
-/*Esto no anda:*/
+/*  Heroic Reception de Kevin MacLeod está autorizado la licencia Creative Commons Attribution (https://creativecommons.org/licenses/by/4.0/)
+Fuente: http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1100206
+Artista: http://incompetech.com/ */
 
-var horaReloj = document.getElementById("hora"),
-	 minReloj = document.getElementById("min"),
-	 horaAlarma = document.getElementById("horaAlarma"),
-	 minAlarma = document.getElementById("minAlarma");
-	 
 
-function alarma (){
-if (horaReloj == horaAlarma.value && minReloj.value == minAlarma) {
-alert("Alarmaaaaaaaaaaaaaa!");
+var btn = document.getElementById("button"),
+	done = document.getElementById("done"),
+	 settings = document.getElementById("setear");
 
+function verAlarma () {
+settings.className = "seve";
+}
+
+function ocultarBoton () {
+btn.className = "tenue";}
+
+function listo() {
+settings.className = "noseve";
+btn.className = "button";}
+
+
+
+btn.addEventListener("click", verAlarma);
+btn.addEventListener("mouseup", ocultarBoton);
+done.addEventListener("click", listo);
+
+
+function alarma() {
+var tiempo = new Date();	
+var horas = tiempo.getHours(), 
+	minutos = tiempo.getMinutes(),
+	segundos = tiempo.getSeconds(),
+	horaInput = document.getElementById("horaAlarma").value,
+	minInput = document.getElementById("minutoAlarma").value,
+	sonido = document.getElementById("audio");
+	
+if (minInput < 10 && minInput.length == 1) {
+	minInput =  "0"+minInput;
+	}
+if (horaInput < 10 && horaInput.length ==1) {
+	horaInput = "0"+horaInput;
+}
+
+if (horas < 10 ) {
+horas = "0" + horas;
+}
+if (minutos < 10) {
+minutos = "0" + minutos;
+}
+if (segundos < 10 ) {
+segundos = parseInt("0" + segundos);
+}
+
+
+if (horas == horaInput && minutos == minInput && segundos == 00 ) {
+	sonido.play();
+	window.alert("HORA!");
+	
+}
 }
 
 setInterval(alarma, 1000);
